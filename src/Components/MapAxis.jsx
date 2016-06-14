d45756e27102535b065470bc438116a2cbcc2da2
djvanderlaan <djvanderlaan@unrealizedtime.nl>
2016-06-14 15:23:12 +0200

    Use react-select in MapAxis

--- src/Components/MapAxis.jsx
+++ src/Components/MapAxis.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
-
-const EMPTY = "";
+import Select from 'react-select';
 
 class MapAxis extends React.Component {
   
@@ -10,51 +9,25 @@ class MapAxis extends React.Component {
   }
 
   handleVariableChange(event) {
-    var value = event.target.value === EMPTY ? undefined : event.target.value;
+    console.log(event);
     this.props.onChange({
       name: this.props.axis.name, 
-      value: value
+      value: event ? event.value : undefined
     });
   }
-
-/*  
-  componentWillMount(){
-    componentWillReceiveProps(this.props);
-  }
-  
-  componentWillReceiveProps(props){
-    const {axis, onChange, variables} = props;
-    if (  axis.required 
-       && typeof onChange === "function"
-       ){
-      onChange({name: axis.name, value: variables[0].name})
-    }
-  }
-*/
 
   render() {
-    // create list with options = variables
     const {axis, variables, selection, value} = this.props;
-     
+    const selected_var = selection[axis.name];
     const options = variables.map(function(v) {
-      return (<option key={v.name} value={v.name}>{v.title}</option>);
+      return {value: v.name, label: v.title};
     });
-    
-    //let selected_var = (value && value.name)? value.name : EMPTY
-    let selected_var = selection[axis.name] || EMPTY
-    
-    // add empty option to front of list
-    if (!axis.required){
-      options.unshift(<option key={-1} value={EMPTY}>No variable selected</option>);
-    }
-    
     // create select
     return (
       <div className="axis">
         <h3>{axis.title}</h3>
-        <select value={selected_var} onChange={this.handleVariableChange}>
-          {options}
-        </select>
+        <Select name="filter-categories" value={selected_var} options={options}
+          clearable={!axis.required} onChange={this.handleVariableChange}/>
       </div>
     );
   }
