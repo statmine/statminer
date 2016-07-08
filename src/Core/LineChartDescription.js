26b2d6994094a7d821f28b969c4f064eb9afbf67
djvanderlaan <djvanderlaan@unrealizedtime.nl>
2016-07-08 08:45:32 +0200

    Moved graph descriptions to separate files

---
+++ src/Core/LineChartDescription.js
@@ -0,0 +1,43 @@
+
+let linechart_description = {
+  name : "linechart",
+  title : "Line chart",
+  graph : grph.line(),
+  can_draw : function(schema, data, mapping) {
+    // schema, data and mapping are required for drawing
+    if (schema === undefined || data === undefined || mapping === undefined)
+      return false;
+    // check required axes
+    if (mapping.x === undefined || mapping.y === undefined)
+      return false;
+    // TODO?: check types
+    return true;
+  },
+  axes: [
+    {
+      name : 'x',
+      title : 'X-Axis',
+      accepts : ['date'],
+      required: true
+    }, {
+      name : 'y',
+      title : 'Y-Axis',
+      accepts: ['number'],
+      required: true
+    }, {
+      name : 'colour',
+      title : 'Colour',
+      accepts: ['categorical', 'date']
+    }, {
+      name : 'column',
+      title : 'Columns',
+      accepts: ['categorical', 'date']
+    }, {
+      name : 'row',
+      title : 'Rows',
+      accepts: ['categorical', 'date']
+    }
+  ]
+};
+
+export default linechart_description;
