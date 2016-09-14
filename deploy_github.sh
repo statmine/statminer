5cf8c470868742b2c7eca5e661e456bf2ffdaeaa
Edwin de Jonge <edwindjonge@gmail.com>
2016-09-14 15:46:50 +0200

    tweaking

--- deploy_github.sh
+++ deploy_github.sh
@@ -1,6 +1,8 @@
 git checkout gh-pages
 git merge dev
 npm run build
-git commit -am 'deploy to github $(date +%F)'
-git push
-git checkout dev
+MESSAGE: "deploy to github $(date +%F)"
+echo $MESSAGE
+#git commit -am 'deploy to github $(date +%F)'
+#git push
+#git checkout dev
